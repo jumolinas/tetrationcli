653143b755a1eca2cd624a4ee5cc03ac5914b1d2
jmolinas <jmolinas@cisco.com>
2018-11-19 22:04:43 +0100

    Application creation and deletion working Scopes list and delete done

--- tetrationcli/controllers/applications.py
+++ tetrationcli/controllers/applications.py
@@ -1,5 +1,6 @@
 from cement import Controller, ex
 import json
+import pickle
 
 class Applications(Controller):
     
@@ -7,52 +8,82 @@ class Applications(Controller):
         label = 'applications'
         stacked_type = 'nested'
         # stacked_on = 'base'
-
-    def restclient(self, http, query, json_value=None):
-        restclient = self.app.tetpyclient
-        if http in 'GET':
-            response = restclient.get(query)
-        if http in 'POST':
-            response = restclient.post(query)
-        if http in 'DELETE':
-            response = restclient.delete(query)
-        if http in 'PUT':
-            response = restclient.put(query,json_body=json_value)
-        else:
-            response = None
-        self.app.log.debug('command returned: %s' % response.status_code)
-        data = {}
-        data['results'] = json.loads(response.content.decode("utf-8"))
-        self.app.log.debug('data returned: %s' % data)
-        return data
     
     @ex(help='list applications', arguments=[
-            (
-                ['-all'],
-                    {'help': 'Show more columns', 'action': 'store_true', 'dest': 'all'}
-            )
+            (['-all'],
+                    {'help': 'Show more columns', 'action': 'store_true', 'dest': 'all'})
     ])
     def list(self):
         """
         Return the list of all the applications
         """
+        restclient = self.app.tetpyclient
+        response = restclient.get('/applications')
+        content = json.loads(response.content.decode("utf-8"))
+        self.app.log.debug('command returned: %s' % response.status_code)
+        self.app.log.debug('data returned: %s' % content)
         data = {
             'all' : self.app.pargs.all,
-            'restclient_data': self.restclient('GET','/applications')
+            'results': content
         }
 
         self.app.render(data, 'applications_list.jinja2')
 
-    @ex(help='create')
+    @ex(help='Create new Application from file', arguments=[
+        (['-template'],
+            {'help': 'Generate template to create the Application', 'action': 'store_true', 'dest': 'template'}),
+        (['-file'],
+            {'help': 'File to generate the Application', 'action': 'store', 'dest': 'file'}),
+    ])
     def create(self):
         """
-
+        POST /openapi/v1/applications
         """
-        pass
-    
-    @ex(help='delete')
-    def remove(self):
-        pass
+        if self.app.pargs.template:
+            template = {
+                'app_scope_id': 'The scope ID to assign to the application',
+                'name': 'A name for the application',
+                'description': 'A description for the application',
+                'alternate_query_mode' : 'false',
+                'strict_validation': 'false',
+                'primary': 'true'
+            }
+            with open('template_create_application.json', 'w') as fp:
+                json.dump(template, fp, indent=4)
+        elif self.app.pargs.file:
+            content = ''
+            with open(self.app.pargs.file) as fp:
+                content = json.loads(fp.read())
+            self.app.log.debug('File content {0}'.format(content))
+            restclient = self.app.tetpyclient
+            response = restclient.post('/applications',json_body=json.dumps(content))
+            self.app.log.debug('Response {0} Message {1}'.format(response.status_code,response.content))
+            if response.status_code > 299:
+                error_message = json.loads(response.text)
+                self.app.log.error('Error {0} > {1}'.format(error_message['status'], error_message['error']))
+            else:
+                self.app.log.info('Application {0} Created on Scope {1}'.format(content['name'],content['app_scope_id']))
+
+    @ex(help='delete application', arguments=[
+            (['-application'],
+                {'help': 'Application ID', 'action': 'store', 'dest': 'application_id'})
+    ])
+    def delete(self):
+        """
+        DELETE /openapi/v1/applications/{application_id}
+        """
+        data = {
+            'application_id': self.app.pargs.application_id
+        }
+        restclient = self.app.tetpyclient
+        response = restclient.delete('/applications/{0}'.format(data['application_id']))
+        self.app.log.debug('command returned: %s' % response.status_code)
+        if response.status_code in [200,204]:
+            self.app.log.info('Deleted Application {0}'.format(data['application_id']))
+        else:
+            error_message = json.loads(response.text)
+            self.app.log.error('Error: {0} \n> Message {1}'.format(error_message['status'],
+                                                                error_message['error']))
 
     @ex(help='update', arguments=[
             (['-application'], 
@@ -66,6 +97,7 @@ class Applications(Controller):
     ],)
     def update(self):
         """
+        TODO: applications update
         {
             "name": "Updated Name",
             "description": "Updated Description",
@@ -82,15 +114,16 @@ class Applications(Controller):
         self.app.log.debug('Parameter: description = %s' % update_application_description)
         self.app.log.debug('Parameter: primary = %s' % update_application_primary)
 
-        req_payload = {
+        data = {
             "name": update_application_name,
             "description": update_application_description,
             "primary": update_application_primary
         }
 
-        restclient_data = self.restclient('PUT', 
-                                        '/applications/%s' % update_application_id,
-                                        json.dumps(req_payload))
+        # restclient_data = self.restclient('PUT', 
+        #                                 '/applications/%s' % update_application_id,
+        #                                 json.dumps(req_payload))
+        self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
 
 
     @ex(help='details', arguments=[
@@ -99,52 +132,48 @@ class Applications(Controller):
     ],)
     def details(self):
         """
+        TODO: applications details
         """
         data = {
             'details_application_id': self.app.pargs.application_id
         }
-
-        self.app.log.debug('Parameter: details_application_id = %s' % data['details_application_id'])
-        data['restclient_data'] = self.restclient('GET', '/applications/%s/details' % data['details_application_id'])
-        self.app.render(data, 'applications_details_list.jinja2')
-
-    @ex(help='delete', arguments=[
-        (['-application'], 
-            {'help': 'Application ID to delete', 'action': 'store', 'dest': 'application_id'})
-    ],)
-    def delete(self):
-        data = {
-            'delete_application_id': self.app.pargs.application_id
-        }
-
-        self.app.log.debug('Parameter: enforce_application_id = %s' % data['delete_application_id'])
-        restclient_data = self.restclient('DELETE', 
-                                '/applications/%s/enable_enforce' % data['delete_application_id'])
-
+        
+        # self.app.log.debug('Parameter: details_application_id = %s' % data['details_application_id'])
+        # data['restclient_data'] = self.restclient('GET', '/applications/%s/details' % data['details_application_id'])
+        # self.app.render(data, 'applications_details_list.jinja2')
+        self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
 
     @ex(help='enforce', arguments=[
         (['-application'], 
             {'help': 'Application ID to enforce', 'action': 'store', 'dest': 'application_id'})
     ],)
     def enforce(self):
+        """
+        TODO: applications enforce
+        """
         data = {
             'enforce_application_id': self.app.pargs.application_id
         }
-
+        
         self.app.log.debug('Parameter: enforce_application_id = %s' % data['enforce_application_id'])
-        restclient_data = self.restclient('POST', 
-                                    '/applications/%s/enable_enforce' % data['enforce_application_id'])
+        # restclient_data = self.restclient('POST', 
+        #                             '/applications/%s/enable_enforce' % data['enforce_application_id'])
+        self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
         
     @ex(help='disable', arguments=[
         (['-application'], 
             {'help': 'Application ID to disable', 'action': 'store', 'dest': 'application_id'})
     ],)
     def disable(self):
+        """
+        TODO: applications disable
+        """
         data = {
             'disable_application_id': self.app.pargs.application_id
         }
 
         self.app.log.debug('Parameter: disable_application_id = %s' % data['disable_application_id'])
-        restclient_data = self.restclient('POST', 
-                                '/applications/%s/disable_enforce' % data['disable_application_id'])
+        # restclient_data = self.restclient('POST', 
+        #                         '/applications/%s/disable_enforce' % data['disable_application_id'])
+        self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
         
--- tetrationcli/controllers/scopes.py
+++ tetrationcli/controllers/scopes.py
@@ -8,25 +8,85 @@ class Scopes(Controller):
         stacked_type = 'nested'
         # stacked_on = 'base'
 
-    @ex(help='list scopes')
+    @ex(help='list scopes', arguments=[
+        (['-a', '--all'],
+            {'help': 'show more columns', 'action': 'store_true', 'dest': 'all'})
+    ])
     def list(self):
         restclient = self.app.tetpyclient
+        show_all = False
+        if self.app.pargs.all:
+            show_all = True
         response = restclient.get('/app_scopes')
         self.app.log.debug('command returned: %s' % response.status_code)
         data = {
-            'results': json.loads(response.content.decode("utf-8"))
+            'results': json.loads(response.content.decode("utf-8")),
+            'all': show_all
         }
         
         self.app.log.debug('Scopes Data: %s' % data)
         self.app.render(data, 'scopes_list.jinja2')
 
-    @ex(help='create')
+    @ex(help='create new scope', arguments=[
+        (['--name', '-n'],
+            {'help': 'Name of the scope', 'action': 'store', 'dest': 'name'}),
+        (['--query', '-q'],
+            {'help': 'Query in JSON format', 'action': 'store', 'dest': 'query'}),
+        (['--parent', '-p'],
+            {'help': 'Parent Scope ID', 'action': 'store', 'dest': 'parent'}),
+    ])
     def create(self):
+        """
+        TODO: scopes create
+        {
+            "short_name": "App Scope Name",
+            "short_query": {"type":"eq",
+                "field":"ip",
+                "value": <....>
+            },
+            "parent_app_scope_id": <parent_app_scope_id>
+        }
+        """
+        restclient = self.app.tetpyclient
+        data = {
+            'short_name': self.app.pargs.name,
+            'short_query': json.loads(self.app.pargs.query),
+            'parent_app_scope_id': self.app.pargs.parent
+        }
+
+        # response = restclient.post('/app_scopes', json_body=json.dumps(data))
+        # self.app.log.debug('Response {0}: {1}'.format(response.status_code, 
+        #                                             response.content.decode("utf-8")))
+        
         self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
     
-    @ex(help='delete')
-    def remove(self):
-        self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
+    @ex(help='delete scope', arguments=[
+        (['--scope', '-s'], 
+            {'help': 'Scope ID to Remove', 'action': 'store', 'dest': 'scope_id'})
+    ])
+    def delete(self):
+        """
+        DELETE /openapi/v1/app_scopes/{app_scope_id}
+        """
+        restclient = self.app.tetpyclient
+        data = {
+            'scope_id': self.app.pargs.scope_id
+        }
+        response = restclient.delete('/app_scopes/{0}'.format(data['scope_id']))
+        self.app.log.debug('Response {0}: {1}'.format(response.status_code, 
+                                                    response.content.decode("utf-8")))
+        if response.status_code == 422:
+            error_details = json.loads(response.content.decode("utf-8"))
+            error_message = '{0} \nDetails:\n'.format(error_details['error'])
+            
+            for detail in error_details['details']:
+                error_message += '> {0} \n> Dependencies:\n'.format(detail['error'])
+                for dependency in detail['dependents']:
+                    error_message += '>> {0} {1}'.format(dependency['id'], dependency['name'])
+                    
+            self.app.log.error(error_message)
+        else:
+            self.app.log.info('Scope {0} successfully deleted'.format(data['scope_id']))
 
     @ex(help='update')
     def update(self):
