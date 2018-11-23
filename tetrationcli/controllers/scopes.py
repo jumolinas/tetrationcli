7549b456c8851eb94ddb95b309298380b521335c
jmolinas <jmolinas@cisco.com>
2018-11-23 20:04:12 +0100

    Finished scopes manipulation via API

--- tetrationcli/controllers/scopes.py
+++ tetrationcli/controllers/scopes.py
@@ -9,19 +9,20 @@ class Scopes(Controller):
         # stacked_on = 'base'
 
     @ex(help='list scopes', arguments=[
-        (['-all'],
-            {'help': 'show more columns', 'action': 'store_true', 'dest': 'all'})
+        # (['-all'],
+        #     {'help': 'show more columns', 'action': 'store_true', 'dest': 'all'})
+        # TODO: Create option for extra columns
     ])
     def list(self):
         restclient = self.app.tetpyclient
         show_all = False
-        if self.app.pargs.all:
-            show_all = True
+        # if self.app.pargs.all:
+        #     show_all = True
         response = restclient.get('/app_scopes')
         self.app.log.debug('command returned: %s' % response.status_code)
         data = {
             'results': json.loads(response.content.decode("utf-8")),
-            'all': show_all
+            'all': False
         }
         
         self.app.log.debug('Scopes Data: %s' % data)
@@ -37,7 +38,6 @@ class Scopes(Controller):
     ])
     def create(self):
         """
-        TODO: scopes create
         {
             "short_name": "App Scope Name",
             "short_query": {"type":"eq",
@@ -48,17 +48,30 @@ class Scopes(Controller):
         }
         """
         restclient = self.app.tetpyclient
-        data = {
-            'short_name': self.app.pargs.name,
-            'short_query': json.loads(self.app.pargs.query),
-            'parent_app_scope_id': self.app.pargs.parent
-        }
+        data = {}
+        query_check = json.loads(self.app.pargs.query)
 
-        # response = restclient.post('/app_scopes', json_body=json.dumps(data))
-        # self.app.log.debug('Response {0}: {1}'.format(response.status_code, 
-        #                                             response.content.decode("utf-8")))
+        if query_check['type'] is None or query_check['field'] is None or query_check['value'] is None:
+            query_tmplate = '{ "type": "eq", "field": "ip", "value": <...> }'
+            self.app.log.error('Incorrect query: as example {0}'.format(query_tmplate))
+            
+        else:
+            data = {
+                'short_name': self.app.pargs.name,
+                'short_query': query_check,
+                'parent_app_scope_id': self.app.pargs.parent
+            }
+
+        response = restclient.post('/app_scopes', json_body=json.dumps(data))
+        self.app.log.debug('Response {0}: {1}'.format(response.status_code, 
+                                                    response.content.decode("utf-8")))
+
+        ret_content = json.loads(response.content.decode('utf-8'))
         
-        self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
+        if response.status_code > 299:
+            self.app.log.error('{0} - {1}'.format(ret_content['status'], ret_content['error']))
+        else:
+            self.app.log.info('Scope {} created successfully'.format(ret_content['name']))
     
     @ex(help='delete scope', arguments=[
         (['-scope'], 
