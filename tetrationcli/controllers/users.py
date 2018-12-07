ec93c80522f079ccdf7ffcc8e8564aa8b92aeaa9
jmolinas <jmolinas@cisco.com>
2018-12-07 15:11:07 +0100

    Added the roles the user belongs to.

--- tetrationcli/controllers/users.py
+++ tetrationcli/controllers/users.py
@@ -12,6 +12,9 @@ class Users(TetController):
     @ex(help='list users')
     def list(self):
         response = self.tetration().get('/users')
+        response_roles = self.tetration().get('/roles')
+        data_roles = json.loads(response_roles.content.decode("utf-8"))
+        dict_roles = {r['id']: r['name'] for r in data_roles}
         self.app.log.debug('{0} - {1}'.format(response.status_code,
                                                 response.content.decode('utf-8')))
         
@@ -22,10 +25,11 @@ class Users(TetController):
         
         data = json.loads(response.content.decode("utf-8"))
         
-        headers = ['User ID', 'Name', 'e-Mail']
+        headers = ['User ID', 'Name', 'e-Mail', 'Roles']
         data_list = [[x['id'],
                     '{0} {1}'.format(x['first_name'],x['last_name']),
-                    x['email']] for x in data ]
+                    x['email'],
+                    ','.join([dict_roles[y] for y in x['role_ids']])] for x in data ]
 
         self.app.render(data_list, headers=headers)
 
